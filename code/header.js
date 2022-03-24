ca204351ae41cafb7401620fe11e47ff3ef7246f
daridev <hernandezdarifrancisco@gmail.com>
2022-03-24 06:36:31 -0600

    header responsive toggle

---
+++ code/header.js
@@ -0,0 +1,21 @@
+const header_menu_button = document.querySelector ("header button.menu")
+const header_menu_close = document.querySelector ("header li.close")
+const header_menu = document.querySelector ("header nav")
+
+console.log ()
+
+// Open menu
+header_menu_button.addEventListener ("click", function (e) {
+    header_menu.classList.remove ("collapsed")
+})
+
+// Fix link
+header_menu.addEventListener ("click", function (e) {
+    console.log (["li", "use", "svg"].includes (e.target.tagName.toLowerCase()))
+    console.log (e.target.tagName)
+})
+
+// Close menu
+header_menu_close.addEventListener ("click", function (e) {
+    header_menu.classList.add ("collapsed")
+})
