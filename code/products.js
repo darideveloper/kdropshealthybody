74df19dc7a16fd233e285cada73647778f0f9378
daridev <hernandezdarifrancisco@gmail.com>
2022-03-27 00:12:33 -0600

    product modal close it with keyboard and out-click

--- code/products.js
+++ code/products.js
@@ -7,7 +7,6 @@ const products_close_btns = document.querySelectorAll (".products article.modal 
 for (let product_btn of products_btns) {
     product_btn.addEventListener ("click", function (e) {
         let product_number = (product_btn.classList[1].charAt(product_btn.classList[1].length-1)) - 1
-        console.log (product_number)
 
         products_modals_wrapper.classList.remove ("hide")
         products_modals[product_number].classList.remove ("hide")
@@ -17,11 +16,30 @@ for (let product_btn of products_btns) {
 // Add event to ech close button, for close modals
 for (let product_close_btn of products_close_btns) {
     product_close_btn.addEventListener ("click", function () {
-        products_modals_wrapper.classList.add ("hide")
-
-        for (let product_modal of products_modals) {
-            product_modal.classList.add ("hide")
-        }
-
+        products_exist_modal ()
     })
+}
+
+// Detect event key "scape" for close modal
+document.addEventListener('keydown', (event) => {
+        
+    if (event.key === 'Escape') {
+        products_exist_modal ()
+    }
+});
+
+// Detect outside click for close modal
+products_modals_wrapper.addEventListener ("click", function (e) {
+    if (e.target.classList.contains ("wrapper-modals")) {
+        products_exist_modal ()
+    }
+})
+
+function products_exist_modal () {
+    // Function for close modal
+    products_modals_wrapper.classList.add ("hide")
+
+    for (let product_modal of products_modals) {
+        product_modal.classList.add ("hide")
+    }
 }
